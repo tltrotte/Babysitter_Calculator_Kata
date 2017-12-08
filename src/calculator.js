901863317ff5f5a5a629c998c0948e697467a9ad
Tamara Trotter <tltrotte@me.com>
2017-12-07 23:05:47 -0500

    Past test with proper format and all functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,74 +1,69 @@
-
-
 //We know the following variables for the application
- var payPerHour = {
-   beforeSleep: 12,
-   whenSleeping: 8,
-   afterMidnight: 16
- };
+var payPerHour = {
+  beforeSleep: 12,
+  whenSleeping: 8,
+  afterMidnight: 16
+};
 
- var shift = {
-   start: 17,
-   midnight:0,
-   end: 4,
- };
+var shift = {
+  start: 17,
+  midnight: 0,
+  end: 4
+};
 
- // var startTime = time(startTime);
- // var bedTime = time(bedTime);
- // var endTime = time(endTime);
+//This Function Formats the time correctly as well as set the expectaions of time returned on a 24 hour clock for this application.
 
-
- //This Function Formats the time correctly as well as set the expectaions of time returned on a 24 hour clock for this application.
-
- export function time(hour){
-   if(hour.indexOf('00') === -1 ){
-      hour = Number(hour.split(":").shift()) +1;
-    } else {
-       hour = Number(hour.split(":").shift());
-      }
-      if(hour <= 0 || hour > 24)
- 		{
- 			return false;
- 		}else{
-       return hour;
- 	}
- }
- // This is the function used to calculate the hours before Bed.
- export function beforeBedPay(startTime, bedTime)
-  {
-    if(bedTime > startTime && bedTime > shift.midnight)
-    {
-      return ((bedTime - startTime) * payPerHour.beforeSleep);
-
-    } else if(bedTime <= startTime){
-      return 0;
-    }
+export function time(hour) {
+  if (hour.indexOf('00') === -1) {
+    hour = Number(hour.split(":").shift()) + 1;
+  } else {
+    hour = Number(hour.split(":").shift());
   }
+  if (hour <= 0 || hour > 24) {
+    return false;
+  } else {
+    return hour;
+  }
+}
+// This is the function used to calculate the hours before Bed.
+export function beforeBedPay(startTime, bedTime) {
+  if (bedTime > startTime && bedTime > shift.midnight) {
+    return ((bedTime - startTime) * payPerHour.beforeSleep);
+
+  } else if (bedTime <= startTime) {
+    return 0;
+  }
+}
 // This is the function used to calculate the hours during sleeping.
-  export function bedTimePay(startTime, bedTime, endTime)
-   {
-     if(bedTime > startTime && bedTime > shift.midnight && bedTime <= endTime)
-     {
-       return ((endTime-bedTime) * payPerHour.whenSleeping);
+export function bedTimePay(startTime, bedTime, endTime) {
+  if (bedTime > startTime && bedTime > shift.midnight && bedTime <= endTime) {
+    return ((endTime - bedTime) * payPerHour.whenSleeping);
 
-     } else if(bedTime === startTime && endTime >= shift.midnight){
-       return ((endTime - startTime) * payPerHour.whenSleeping);
-     } else if (bedTime <= startTime && endTime < shift.midnight){
-       return  ((24 - startTime) * payPerHour.whenSleeping);
-   }else if(bedTime <= startTime && endTime >= shift.midnight){
-     return ((endTime - startTime) * payPerHour.whenSleeping);
-   } else {
-       return 0;
-     }
-   }
+  } else if (bedTime === startTime && endTime >= shift.midnight) {
+    return ((endTime - startTime) * payPerHour.whenSleeping);
+  } else if (bedTime <= startTime && endTime < shift.midnight) {
+    return ((24 - startTime) * payPerHour.whenSleeping);
+  } else if (bedTime <= startTime && endTime >= shift.midnight) {
+    return ((endTime - startTime) * payPerHour.whenSleeping);
+  } else {
+    return 0;
+  }
+}
 
-   //This is the Function that calculates the pay for hours after midnight.
+//This is the Function that calculates the pay for hours after midnight.
 
-   export function afterMidnightPay (startTime, endTime ) {
-     if(startTime=== 0 || startTime >shift.end && endTime <= shift.end){
-       return endTime * payPerHour.afterMidnight;
-     }
-      else {
-        return 0;
-      }
-    }
+export function afterMidnightPay(startTime, endTime) {
+  if (startTime === 0 || startTime > shift.end && endTime <= shift.end) {
+    return endTime * payPerHour.afterMidnight;
+  } else {
+    return 0;
+  }
+}
+//This is the function that calculates the final pay for the Babysitter Calculator
+
+export function finalPay(start, middle, end) {
+  var startTime = time(start);
+  var bedTime = time(middle);
+  var endTime = time(end);
+  return (beforeBedPay(startTime, bedTime) + bedTimePay(startTime, bedTime, endTime) + afterMidnightPay(startTime, endTime));
+}
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,45 +1,48 @@
-import {time, beforeBedPay, bedTimePay, afterMidnightPay} from "./calculator";
+import {time, beforeBedPay, bedTimePay, afterMidnightPay, finalPay} from "./calculator";
 
-
-
-
-
-
-describe("time function", function(){
+describe("time function", function() {
   //This test check's to make sure that the time is a number
   it('the type of time should equal a number', function() {
-     expect(typeof(time('10:45'))).toEqual('number');
-   });
-//This test makes sure that the time is returned as its self
+    expect(typeof(time('10:45'))).toEqual('number');
+  });
+  //This test makes sure that the time is returned as its self
   it('should return 10 when time is 10:00)', function() {
     expect(time('10:00')).toBe(10);
   });
   //This test make sure that number rounds up when there the minutes are not "00"
- it('should return 22:00 when time is 21:01', function() {
-   expect(time('21:01')).toBe(22);
+  it('should return 22:00 when time is 21:01', function() {
+    expect(time('21:01')).toBe(22);
   });
 });
 //This test makes the calculation for pay before bed time
-describe("beforeBedPay function", function(){
-it('should return $36 if start time is 5pm and bedtime is 8pm', function() {
-     expect(beforeBedPay(17, 20)).toBe(36);
+describe("beforeBedPay function", function() {
+  it('should return $36 if start time is 5pm and bedtime is 8pm', function() {
+    expect(beforeBedPay(17, 20)).toBe(36);
   });
 });
 //This test makes the calculation for bed time hours
-describe("bedTime function", function(){
-it('should return 24 if start time is 6pm  and bedtime is 8pm and endTime 11pm', function() {
-     expect(bedTimePay(18, 20, 23)).toBe(24);
+describe("bedTime function", function() {
+  it('should return 24 if start time is 6pm  and bedtime is 8pm and endTime 11pm', function() {
+    expect(bedTimePay(18, 20, 23)).toBe(24);
   });
-it('should return 16 if start time is 6pm  and bedtime is 6pm endTime 8pm', function() {
-       expect(bedTimePay(18, 18, 20)).toBe(16);
+  it('should return 16 if start time is 6pm  and bedtime is 6pm endTime 8pm', function() {
+    expect(bedTimePay(18, 18, 20)).toBe(16);
   });
-it('should return 24 if start time is 8pm  and bedtime is 6pm endTime 11pm', function() {
-       expect(bedTimePay(20, 18, 23)).toBe(24);
+  it('should return 24 if start time is 8pm  and bedtime is 6pm endTime 11pm', function() {
+    expect(bedTimePay(20, 18, 23)).toBe(24);
   });
 });
 //This test makes the calculation for midnight pay hours.
-describe('afterMidnightPay function', function(){
-   it('should return 48 if start time is midnight and end is 3am', function() {
-     expect(afterMidnightPay(0, 3)).toBe(48);
-   });
+describe('afterMidnightPay function', function() {
+  it('should return 48 if start time is midnight and end is 3am', function() {
+    expect(afterMidnightPay(0, 3)).toBe(48);
+  });
+  it('should return 32 if start time is 10 and end is 2am', function() {
+    expect(afterMidnightPay(10, 2)).toBe(32);
+  });
+});
+describe('total cost for baby sitter', function() {
+  it('should return 72 if start time is 9, bedtime is 11pm, and end is 3am', function() {
+    expect(finalPay("21:00", "23:00", "3:00")).toBe(72);
+  });
 });
