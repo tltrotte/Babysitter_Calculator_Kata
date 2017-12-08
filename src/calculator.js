6f73de7f26f64ef554004fb51260c20ce34e4b55
Tamara Trotter <tltrotte@me.com>
2017-12-07 22:53:35 -0500

    Test Past when shift starts before midnight

--- src/calculator.js
+++ src/calculator.js
@@ -9,7 +9,7 @@
 
  var shift = {
    start: 17,
-   midnight:24,
+   midnight:0,
    end: 4,
  };
 
@@ -36,7 +36,7 @@
  // This is the function used to calculate the hours before Bed.
  export function beforeBedPay(startTime, bedTime)
   {
-    if(bedTime > startTime && bedTime < shift.midnight)
+    if(bedTime > startTime && bedTime > shift.midnight)
     {
       return ((bedTime - startTime) * payPerHour.beforeSleep);
 
@@ -47,15 +47,15 @@
 // This is the function used to calculate the hours during sleeping.
   export function bedTimePay(startTime, bedTime, endTime)
    {
-     if(bedTime > startTime && bedTime < shift.midnight && bedTime <= endTime)
+     if(bedTime > startTime && bedTime > shift.midnight && bedTime <= endTime)
      {
        return ((endTime-bedTime) * payPerHour.whenSleeping);
 
-     } else if(bedTime === startTime && endTime <= shift.midnight){
+     } else if(bedTime === startTime && endTime >= shift.midnight){
        return ((endTime - startTime) * payPerHour.whenSleeping);
-     } else if (bedTime <= startTime && endTime > shift.midnight){
-       return  ((shift.midnight - startTime) * payPerHour.whenSleeping);
-   }else if(bedTime <= startTime && endTime <= shift.midnight){
+     } else if (bedTime <= startTime && endTime < shift.midnight){
+       return  ((24 - startTime) * payPerHour.whenSleeping);
+   }else if(bedTime <= startTime && endTime >= shift.midnight){
      return ((endTime - startTime) * payPerHour.whenSleeping);
    } else {
        return 0;
